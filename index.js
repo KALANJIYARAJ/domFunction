724742bd4a95c66ab6b64835f1ba634c3e1efb3b
KALANJIYARAJ <110882243+KALANJIYARAJ@users.noreply.github.com>
2022-09-02 10:52:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+var L1=label("label","for","text1","FirstName");
+var br1=linebreaker("br");
+var I1=inputelement("input","type","text","id","text1");
+var br2=linebreaker("br");
+document.body.append(L1,br1,I1,br2);
+
+var L1=label("label","for","text2","MiddletName");
+var br1=linebreaker("br");
+var I1=inputelement("input","type","text","id","text2");
+var br2=linebreaker("br");
+document.body.append(L1,br1,I1,br2);
+
+var L1=label("label","for","text3","LastName");
+var br1=linebreaker("br");
+var I1=inputelement("input","type","text","id","text3");
+var br2=linebreaker("br");
+document.body.append(L1,br1,I1,br2);
+
+var L1=label("label","for","email","Email");
+var br1=linebreaker("br");
+var I1=inputelement("input","type","email","id","email");
+var br2=linebreaker("br");
+document.body.append(L1,br1,I1,br2);
+
+var L1=label("label","for","text4","Contact");
+var br1=linebreaker("br");
+var I1=inputelement("input","type","text","id","text4");
+var br2=linebreaker("br");
+document.body.append(L1,br1,I1,br2);
+
+
+
+
+//label function
+function label(tagname,attrname,attrvalue,content){
+var ele=document.createElement(tagname);
+ ele.setAttribute(attrname,attrvalue);
+ ele.innerHTML=content;
+ return ele;
+ //<label for="email">Email</label>
+ //<div class="container">this is container</div>
+}
+//linebreaker
+function linebreaker(element){
+var ele=document.createElement(element);
+return ele;
+}
+
+//inputelement
+function inputelement(tagname,attrname,attrvalue,attrname1,attrvalue1){
+var ele=document.createElement(tagname);
+ele.setAttribute(attrname,attrvalue);
+ele.setAttribute(attrname1,attrvalue1);
+return ele;
+
+}
